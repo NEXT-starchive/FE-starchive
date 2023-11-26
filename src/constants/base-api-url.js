1f8e62aba1376ff6684029a0df906887854878bc
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-11-26 23:16:19 +0900

    add; change base api url

--- src/constants/base-api-url.js
+++ src/constants/base-api-url.js
@@ -1,5 +1,8 @@
 // export const baseApiUrl = "http://localhost:8080";
+// export const baseApiUrl =
+//   "http://ec2-3-39-243-152.ap-northeast-2.compute.amazonaws.com:8080";
+
 export const baseApiUrl =
-  "http://ec2-3-39-243-152.ap-northeast-2.compute.amazonaws.com:8080";
+  "ec2-52-78-149-207.ap-northeast-2.compute.amazonaws.com";
 
 export const redirectUrl = "http://localhost:3000/auth";
